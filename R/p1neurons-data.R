a0a9dc0839522f2843fc218a13a2e1f990332911
Gregory Jefferis <jefferis@gmail.com>
2015-02-04 20:55:40 -0800

    P1s and closely related classes
    * ... they look like P1s by stalk but don't innervate arch/ring

--- R/p1neurons-data.R
+++ R/p1neurons-data.R
@@ -1,10 +1,11 @@
 #' List of 143 P1 neurons from Chiang et al 2011 converted to dotprops objects
 #'
 #' This R list (which has additional class \code{neuronlist}) contains 143
-#' skeletonized \emph{Drosophila} P1 neurons as \code{dotprops} objects.
-#' Original data is due to Chiang et al. 2011, who have generously shared their
-#' raw data at \url{http://flycircuit.tw}. Image registration and further
-#' processing was carried out by Greg Jefferis and Marta Costa.
+#' skeletonized \emph{Drosophila} neurons (including both P1 neurons and closely
+#' related classes) as \code{dotprops} objects. Original data is due to Chiang
+#' et al. 2011, who have generously shared their raw data at
+#' \url{http://flycircuit.tw}. Image registration and further processing was
+#' carried out by Greg Jefferis and Marta Costa.
 #' @name p1dps
 #' @docType data
 #' @references [1] Chiang A.S., Lin C.Y., Chuang C.C., Chang H.M., Hsieh C.H.,
@@ -38,6 +39,29 @@ NULL
 #'   Comput Biol, 8(9):e1002658, Sep 2012. doi: 10.1371/journal.pcbi.1002658.
 #'
 #'   [3] NBLAST: Rapid, sensitive comparison of neuronal structure and
+#'   construction of neuron family databases. Marta Costa, Aaron D. Ostrovsky,
+#'   James D. Manton, Steffen Prohaska, Gregory S.X.E. Jefferis. bioRxiv doi:
+#'   http://dx.doi.org/10.1101/006346.
+NULL
+
+#' All by all nblast score matrix for 143 traced P1 neurons
+#'
+#' This all by all score matrix can be used for structure-based clustering of P1
+#' neurons. The score matrix was calculated by
+#' \code{nat.nblast::nblast_allbyall(p1dps)}. The nblast algorithm is described
+#' in detail in [1].
+#' @name p1scoremat
+#' @docType data
+#' @examples
+#' hc=hclustfc(names(p1dps))
+#' plot(hc)
+#' library(dendroextras)
+#' hcc=colour_clusters(hc, k=2)
+#' labels(hcc)=with(p1dps[labels(hcc)], sex)
+#' plot(hcc)
+#' @seealso \code{\link[nat.nblast]{nhclust}}, \code{\link[flycircuit]{hclustfc}}
+#' @references
+#'   [1] NBLAST: Rapid, sensitive comparison of neuronal structure and
 #'   construction of neuron family databases. Marta Costa, Aaron D. Ostrovsky,
 #'   James D. Manton, Steffen Prohaska, Gregory S.X.E. Jefferis. bioRxiv doi:
 #'   http://dx.doi.org/10.1101/006346.
